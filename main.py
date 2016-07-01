2d04fec8de6281ce57730c238631b8092e526b4b
Andy Casey <andycasey@gmail.com>
2016-07-01 18:15:39 +1000

    Trigger on event payload

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
 
+import dotdraft
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", ])
 def root():
     return render_template("index.html") 
 
+@app.route("/", methods=["POST", ])
+def recieve_payload():
+   if not dotdraft.trigger_payload(request):
+       return render_template("index.html")
