67d7c8c2d627f017e5f65287f17065a1de088a73
Andy Casey <andycasey@gmail.com>
2016-07-01 18:11:36 +1000

    Render GET page correctly

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello, world!"
+def root():
+    return render_template("index.html") 
+
